8ddde1746d2f48be9fef1b671562ab4fb1e51843
Daniel <Daniel@PC-Daniel>
2019-07-19 06:06:24 -0300

    Realizadas melhorias nas urls e foram inseridos alguns filtros que estavam crashando no Pentaho.

--- src/main/java/br/ufrn/imd/webscraping/Scraping.java
+++ src/main/java/br/ufrn/imd/webscraping/Scraping.java
@@ -16,10 +16,11 @@ public class Scraping {
 		
 		try {
 			String url = "https://www.vivareal.com.br/venda/rio-grande-do-norte/natal/apartamento_residencial/?__vt=rpca:a";
+			String urlFiltro = "#onde=BR-Rio_Grande_do_Norte-NULL-Natal&tipos=apartamento_residencial";
 			String arquivo = "resultado-scraping.csv";
 			
 			// buscar o documento por HTTP
-			Document doc = Jsoup.connect(url).get();
+			Document doc = Jsoup.connect(url + urlFiltro).get();
 			
 			int paginaAtual = 1;
 			
@@ -59,36 +60,56 @@ public class Scraping {
 				String vagas = anuncio.select("li.property-card__detail-garage span.property-card__detail-value").text();
 				String preco = anuncio.getElementsByClass("property-card__price").text();
 				
-				writer.append(titulo);
-				writer.append(";");
-				writer.append(endereco);
-				writer.append(";");
-				writer.append(taxa);
-				writer.append(";");
-				writer.append(area);
-				writer.append(";");
-				writer.append(quartos);
-				writer.append(";");
-				writer.append(banheiros);
-				writer.append(";");
-				writer.append(suites);
-				writer.append(";");
-				writer.append(vagas);
-				writer.append(";");
-				writer.append(preco);
-				writer.append("\n");
+				// Só escrever no arquivo os anúncios que terminarem "Natal - RN".
+				// Mesmo filtrando por "Natal - RN" e "Apartamento" no site, estava trazendo anúncios de outros estados e cidades. 
+				if(endereco.endsWith("Natal - RN")) {
+					writer.append(titulo);
+					writer.append(";");
+					
+					// Se endereço não começar com ..., inserir " | ".
+					// Inseri a barra vertical para auxiliar na divisão de logradouro, bairro e cidade no Pentaho.
+					if(!endereco.startsWith("Rua") && !endereco.startsWith("Av") && !endereco.startsWith("R.") 
+							&& !endereco.startsWith("Alameda") && !endereco.startsWith("Travessa") && !endereco.startsWith("Vila")
+							&& !endereco.startsWith("Largo") && !endereco.startsWith("Praça") && !endereco.startsWith("Senador")
+							&& !endereco.startsWith("Passagem") && !endereco.startsWith("1ª Travessa")) {
+						writer.append(" | ");
+						writer.append(endereco);
+						
+					// Foi necessário a condição porque havia um anúncio com ";", delimitador utilizado para separar os campos.
+					} else if (endereco.startsWith("Rua;")) {
+						writer.append(endereco.replace("Rua;", "Rua"));
+					} else {
+						writer.append(endereco);
+					}
+					writer.append(";");
+					writer.append(taxa);
+					writer.append(";");
+					writer.append(area);
+					writer.append(";");
+					writer.append(quartos);
+					writer.append(";");
+					writer.append(banheiros);
+					writer.append(";");
+					writer.append(suites);
+					writer.append(";");
+					writer.append(vagas);
+					writer.append(";");
+					writer.append(preco);
+					writer.append("\n");
+				}
 				
 				if(i % 35 == 0 && i != 0){
 					writer.flush();
 					
 					System.out.println("Concluída a paginação: " + paginaAtual);
 					
+					// Quando o link próxima página estiver desabilitado, é encerrado o for. 
 					if(proximaPagina.hasAttr("data-disabled")) {
 						break;
 					}
 					
-					String url2 = url + "&pagina=" + ++paginaAtual;
-					doc = Jsoup.connect(url2).get();
+					String urlPagina = url + "&pagina=" + ++paginaAtual + urlFiltro;
+					doc = Jsoup.connect(urlPagina).get();
 					anuncios = doc.getElementsByClass("property-card__container");
 					i = 0;
 				}
